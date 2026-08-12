9597ad0313b63628adc6c91e8cccc76a48a0c94e
Khoi D. Nguyen <khoinguyend1992@gmail.com>
2026-08-12 00:19:24 -0400

    [Major] New detector class
    New detector class to handle different detector configurations

---
+++ detector.py
@@ -0,0 +1,52 @@
+import json
+import numpy as np
+class detector():
+	def __init__(self,json_fn = None,detector = None,distance = None,wavelength= None,shape = None,centeri=None,dqi = None,centerj=None,dqj = None):
+		if json_fn is not None:
+			print('Importing detector config from jsons...')
+			self.init_from_json(json_fn)
+		else:
+			print('Setting detector config from manual entry')
+			self.detector = detector
+			self.distance = distance
+			self.wavelength = wavelength
+			self.shape = shape
+			self.centeri = centeri
+			self.centerj = centerj
+			self.dqi = dqi
+			self.dqj = dqj
+
+		self.make_Q()
+
+	def init_from_json(self,json_fn):
+		#Takes in a pyFAI json and imports
+		with open(json_fn, "r") as file:
+			self.js_data = json.load(file)
+		self.detector = self.js_data['poni']['detector']
+		self.distance = self.js_data['poni']['dist']
+		self.wavelength = self.js_data['poni']['wavelength'] *1e10 #pyFAI outputs in meters. convert to angstrom
+		self.shape = self.js_data['shape']
+		self.dqi = self.js_data['poni']['detector_config']['pixel1']
+		self.centeri = self.js_data['poni']['poni1'] / self.dqi
+		self.dqj = self.js_data['poni']['detector_config']['pixel2']
+		self.centerj = self.js_data['poni']['poni2'] / self.dqj
+
+		print("Detector: {}".format(self.detector))
+		print("distance: {} meters".format(self.distance))
+		print("wavelength: {} meters".format(self.wavelength))
+		print("image shape: {} ".format(self.shape))
+		print("centeri: {}".format(self.centeri))
+		print("centerj: {}".format(self.centerj))
+
+	def make_Q(self):
+		self.qi = (np.arange(0,self.shape[0]) - self.centeri)*self.dqi 
+		self.qi = 2*np.pi/self.wavelength*self.qi/self.distance 
+		self.qj = (np.arange(0,self.shape[1]) - self.centerj)*self.dqj 
+		self.qj = 2*np.pi/self.wavelength*self.qj/self.distance 
+
+		qqi,qqj = np.meshgrid(self.qi,self.qj,indexing = 'ij')
+		self.Q = np.sqrt(qqi**2+ qqj**2)
+		self.Q /= 1e10 #m to angstrom
+
+if __name__ == '__main__':
+	det = detector('saxs_4m_pyfai.json')
